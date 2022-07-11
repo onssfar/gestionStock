4b6dc782221a4edc6d15952d1d78e9df4f10cfb1
onssfar <ons234sfar@gmail.com>
2022-07-11 21:18:27 +0200

    RequestMapping of user and product

--- src/main/java/com/management/demo/controller/ProductController.java
+++ src/main/java/com/management/demo/controller/ProductController.java
@@ -7,8 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.management.demo.entities.Product;
@@ -20,28 +25,34 @@ import com.management.demo.service.ProductService;
 
 @RestController
 @CrossOrigin
+@RequestMapping(value = "/product")
 public class ProductController {
 	
     @Autowired
     ProductService productService;	
     
+    @GetMapping("/getAllProduct")
     private List<Product> getAllProduct(){
     	return productService.getAllProduct();
     }
     
+    @GetMapping("/findProduct/{ productid}")
     private Product getProduct(@PathVariable("productid")long IdProduct) {
     	return productService.getProductById(IdProduct);
     }
+    
+    @DeleteMapping("/deleteProduct/{productid}")
     private void deleteProduct(@PathVariable ("productid") long IdProduct) {
     	productService.delete(IdProduct);
     }
     
-	private ResponseEntity saveProduct(@RequestBody Product product) {
+    @PostMapping("/addProduct")
+	private ResponseEntity AddProduct(@RequestBody Product product) {
 		Product saveProduct = productService.saveProd(product);
 		return new ResponseEntity<>(saveProduct, HttpStatus.OK);
 	}
     
-    
+	@PutMapping("/updateProduct")
     private Product update(@RequestBody Product product ,long IdProduct ) {
     	productService.update(product, IdProduct);
     	return product;
--- src/main/java/com/management/demo/controller/UserController.java
+++ src/main/java/com/management/demo/controller/UserController.java
@@ -7,8 +7,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.management.demo.entities.Product;
@@ -18,28 +23,34 @@ import com.management.demo.service.UserService;
 
 @RestController
 @CrossOrigin
+@RequestMapping(value = "/user")
 public class UserController {
 	
     @Autowired
     UserService userService;	
     
+    @GetMapping("/getAllUser")
     private List<User> getAllProduct(){
     	return userService.getAllUser();
     }
     
+    @GetMapping("/findUser/{idUser}")
     private User getUser(@PathVariable("idUser")long IdUser) {
     	return userService.getUserById(IdUser);
     }
+    
+    @DeleteMapping("/deleteUser/{Userid}")
     private void deleteUserById(@PathVariable ("Userid") long IdUser) {
     	userService.deleteUser(IdUser);
     }
     
-	private ResponseEntity saveProduct(@RequestBody User user) {
+    @PostMapping("/addUser")
+	private ResponseEntity AddUser(@RequestBody User user) {
 		User saveProduct = userService.saveUser(user);
 		return new ResponseEntity<>(saveProduct, HttpStatus.OK);
 	}
     
-    
+    @PutMapping("/updateUser")
     private User update(@RequestBody User user ,long IdUser ) {
     	userService.updateUser(user ,IdUser);
     	return user;
